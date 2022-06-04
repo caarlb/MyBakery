4e7be7601b54acb305f05ed2052e1394de6050d3
caarlb <s.caarlb@gmail.com>
2022-06-03 22:30:21 -0600

    initial commit

---
+++ src/java/Data/clsConection.java
@@ -0,0 +1,20 @@
+
+package Data;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.DriverManager;
+public class clsConection {
+    private String conectionString = "jdbc:sqlserver://localhost;" +
+			"databaseName=DB_BAKER;user=sa;password=Umg$19;";
+    public Connection conectar() throws Exception{
+        Connection cn=null;
+        try{
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            cn = DriverManager.getConnection(conectionString);
+        }catch (SQLException e){
+            throw e;
+        }finally{            
+        }
+        return cn;
+    } 
+  }
